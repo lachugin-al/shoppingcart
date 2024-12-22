8f686581acc3df82bf2dc53476aff2a1e41acc8f
lachugin-al <anton.lachugin.l@gmail.com>
2024-12-22 15:58:34 +0300

    add KDoc for di package

--- app/src/main/kotlin/com/example/shoppingcart/di/AppModule.kt
+++ app/src/main/kotlin/com/example/shoppingcart/di/AppModule.kt
@@ -10,26 +10,52 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+/**
+ * Модуль Dagger Hilt для предоставления зависимостей, связанных с функциональностью корзины.
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    /**
+     * Предоставляет экземпляр [CartRepository] как зависимость.
+     *
+     * @return Новый экземпляр [CartRepository].
+     */
     @Provides
     @Singleton
     fun provideCartRepository(): CartRepository {
         return CartRepository()
     }
 
+    /**
+     * Предоставляет экземпляр [GetCartItemsUseCase].
+     *
+     * @param cartRepository Репозиторий корзины.
+     * @return Экземпляр [GetCartItemsUseCase].
+     */
     @Provides
     fun provideGetCartItemsUseCase(cartRepository: CartRepository): GetCartItemsUseCase {
         return GetCartItemsUseCase(cartRepository)
     }
 
+    /**
+     * Предоставляет экземпляр [UpdateCartItemUseCase].
+     *
+     * @param cartRepository Репозиторий корзины.
+     * @return Экземпляр [UpdateCartItemUseCase].
+     */
     @Provides
     fun provideUpdateCartItemUseCase(cartRepository: CartRepository): UpdateCartItemUseCase {
         return UpdateCartItemUseCase(cartRepository)
     }
 
+    /**
+     * Предоставляет экземпляр [DeleteCartUseCase].
+     *
+     * @param cartRepository Репозиторий корзины.
+     * @return Экземпляр [DeleteCartUseCase].
+     */
     @Provides
     fun provideDeleteCartUseCase(cartRepository: CartRepository): DeleteCartUseCase {
         return DeleteCartUseCase(cartRepository)
