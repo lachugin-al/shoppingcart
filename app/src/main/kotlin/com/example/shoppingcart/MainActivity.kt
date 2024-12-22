8c43428db73a1bd217690ac06d9cb9fd84ad3f28
lachugin-al <anton.lachugin.l@gmail.com>
2024-12-22 15:49:55 +0300

    add MainActivity

--- app/src/main/kotlin/com/example/shoppingcart/MainActivity.kt
+++ app/src/main/kotlin/com/example/shoppingcart/MainActivity.kt
@@ -3,17 +3,16 @@ package com.example.shoppingcart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.shoppingcart.data.CartRepository
 import com.example.shoppingcart.ui.CartScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val cartRepository = CartRepository()
-
         setContent {
-            CartScreen(cartInteractor = cartRepository)
+            CartScreen() // ViewModel будет инжектирован автоматически через Hilt
         }
     }
 }
